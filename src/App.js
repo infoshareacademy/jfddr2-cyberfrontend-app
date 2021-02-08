a5e329cf01efb115c091118240fe47d528bbc6c3
Wiktor Jagiełło <wiktor.jagiello93@gmail.com>
2021-02-08 17:24:03 +0100

    Swap code from index to app

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import './index-reset.css';
+import React from 'react';
+import Column from '../src/views/ProjectView/Column.js';
+import AddNew from '../src/views/ProjectView/AddNew.js';
+
+const App = () => {
+  return (
+    //temporarly className
+    <div className="project-canvas">
+      <Column>
+        <AddNew />
+      </Column>
+      <Column>
+        <AddNew />
+      </Column>
+      <Column>
+        <AddNew />
+      </Column>
+      <Column>
+        <AddNew />
+      </Column>
+      <AddNew />
+    </div>
+  );
+};
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,33 +1,10 @@
-import './index-reset.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Column from '../src/views/ProjectView/Column.js';
-import AddNew from '../src/views/ProjectView/AddNew.js';
-
-const App = () => {
-  return (
-    //temporarly className
-    <div className="project-canvas">
-      <Column>
-        <AddNew />
-      </Column>
-      <Column>
-        <AddNew />
-      </Column>
-      <Column>
-        <AddNew />
-      </Column>
-      <Column>
-        <AddNew />
-      </Column>
-      <AddNew />
-    </div>
-  );
-};
+import App from './App.js';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.querySelector('#root')
 );
