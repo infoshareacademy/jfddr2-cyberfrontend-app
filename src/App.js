e48f8eab2f5d970bda72028f5eac2b2b7b635a24
Wiktor Jagiełło <wiktor.jagiello93@gmail.com>
2021-02-10 14:44:08 +0100

    Rename AddNew and update import

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './index-reset.css';
 import React from 'react';
 import Column from '../src/views/ProjectView/Column.js';
-import AddNew from './views/ProjectView/AddNewColumn.js';
+import AddNewColumn from './views/ProjectView/AddNewColumn.js';
 
 const App = () => {
   return (
@@ -9,7 +9,7 @@ const App = () => {
     <div className="project-canvas">
       <Column label="TODO" />
       <Column />
-      <AddNew />
+      <AddNewColumn />
     </div>
   );
 };
