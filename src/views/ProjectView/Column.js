7a8fea597e8a2903c7690bf5fa97a31986c15a5b
Wiktor Jagiełło <wiktor.jagiello93@gmail.com>
2021-02-10 13:28:59 +0100

    Move column content to seperate component

--- src/views/ProjectView/Column.js
+++ src/views/ProjectView/Column.js
@@ -1,5 +1,6 @@
 import './Column.css';
 import React from 'react';
+import ColumnContent from './ColumnContent';
 
 const Column = ({ label = 'Your Column' }) => {
   return (
@@ -8,7 +9,7 @@ const Column = ({ label = 'Your Column' }) => {
         <input className="column-title" type="text" defaultValue={label} />
         <div className="column-options">...</div>
       </div>
-      <div className="column-content"></div>
+      <ColumnContent />
       <div className="column-footer">
         <div className="add-task">+ Dodaj kolejną task</div>
         <div className="column-options">...</div>
