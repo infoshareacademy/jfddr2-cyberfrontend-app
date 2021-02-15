e3f2d785fb4e8363512f63943640234f53284ca3
Adam <vodka.adam@gmail.com>
2021-02-15 14:17:01 +0100

    firebase app add with firebaseconfig

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
-import './index-reset.css';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import ProjectView from './views/ProjectView/ProjectView';
-import TableView from './views/TableView/TableView';
-import Nav from './views/Nav/Nav';
-import LoginButton from './views/LandingPage/LandingPage'
+import { firebaseConfig } from "../src/firebase/firebaseConfig";
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
+
+import "./index-reset.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import ProjectView from "./views/ProjectView/ProjectView";
+import TableView from "./views/TableView/TableView";
+import Nav from "./views/Nav/Nav";
+import LoginButton from "./views/LandingPage/LandingPage";
+firebase.initializeApp(firebaseConfig);
 
 const App = () => {
   return (
