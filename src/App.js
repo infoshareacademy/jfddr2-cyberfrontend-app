fffb1ec49ede7077a8d3ad0671125403928ca04a
Wiktor Jagiełło <wiktor.jagiello93@gmail.com>
2021-02-21 10:17:49 +0100

    Use trainer code snippet to get list of users

--- src/App.js
+++ src/App.js
@@ -1,28 +1,34 @@
-import { firebaseConfig } from "../src/firebase/firebaseConfig";
-import firebase from "firebase/app";
-import "firebase/firestore";
-import "firebase/auth";
-import "./index-reset.css";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import ProjectView from "./views/ProjectView/ProjectView";
-import TableView from "./views/TableView/TableView";
-import Nav from "./views/Nav/Nav";
-import LoginButton from "./views/LandingPage/LandingPage";
-import { useEffect, useState } from "react";
+import { firebaseConfig } from '../src/firebase/firebaseConfig';
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+import './index-reset.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import ProjectView from './views/ProjectView/ProjectView';
+import TableView from './views/TableView/TableView';
+import Nav from './views/Nav/Nav';
+import LoginButton from './views/LandingPage/LandingPage';
+import { useEffect, useState } from 'react';
 firebase.initializeApp(firebaseConfig);
 
 const App = () => {
-  const [data, setData] = useState({});
+  const [data, setData] = useState([]);
+
   useEffect(() => {
     firebase
       .firestore()
-      .collection("users")
-      .onSnapshot((users) => {
-        users.forEach((user) => {
-          setData(user.data());
+      .collection('users')
+      .onSnapshot((snapshot) => {
+        const users = snapshot.docs.map((user) => {
+          return {
+            ...user.data(),
+            id: user.id,
+          };
         });
+        setData(users);
       });
   }, []);
+
   return (
     <Router>
       <Nav />
