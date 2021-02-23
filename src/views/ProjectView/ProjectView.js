092a9c136e42f80812a5966da86ee9cfa17dd245
Wiktor Jagiełło <wiktor.jagiello93@gmail.com>
2021-02-23 19:04:39 +0100

    Clean up

--- src/views/ProjectView/ProjectView.js
+++ src/views/ProjectView/ProjectView.js
@@ -1,13 +1,13 @@
 import Column from './Column';
 import AddNewColumn from './AddNewColumn';
 
-const ProjectView = ({ data, setData }) => {
+const ProjectView = ({ data }) => {
   const fetchedData = () => {
     if (!data || Object.keys(data).length === 0) {
       return <div>Loading...</div>;
     } else {
-      const columnsInProject = data[0]?.board.project0.projectContent;
-      const userColumns = Object.keys(columnsInProject)?.map((key) => {
+      const columnsInProject = data[0].board.project0.projectContent;
+      const userColumns = Object.keys(columnsInProject).map((key) => {
         return (
           <Column
             key={key}
