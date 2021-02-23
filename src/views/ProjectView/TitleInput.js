b934845d498c5ca6cfec3ba19524d5f945d76f5e
Wiktor Jagiełło <wiktor.jagiello93@gmail.com>
2021-02-23 18:00:52 +0100

    Create component with title edition functionality

---
+++ src/views/ProjectView/TitleInput.js
@@ -0,0 +1,48 @@
+import firebase from 'firebase/app';
+import { useState, useEffect } from 'react';
+
+const TitleInput = ({ index, data, label, visible, setVisible }) => {
+  const [newTitle, setNewTitle] = useState(label);
+  const [currentCollumnContent, setCurrentCollumnContent] = useState({});
+
+  useEffect(() => {
+    const columnsOfUser = data[0].board.project0.projectContent;
+    setCurrentCollumnContent(columnsOfUser);
+  }, [data]);
+
+  const updateTitleHandler = (e) => {
+    if (e.key === 'Enter' && newTitle.trim()) {
+      const id = data[0].id;
+
+      firebase
+        .firestore()
+        .collection('users')
+        .doc(id)
+        .update({
+          'board.project0.projectContent': {
+            ...currentCollumnContent,
+            [index]: {
+              ...currentCollumnContent[index],
+              columnName: newTitle,
+            },
+          },
+        })
+        .then(() => {
+          console.log('Document successfully updated!');
+        });
+      setVisible(!visible);
+    }
+  };
+
+  return (
+    <input
+      type="text"
+      defaultValue={newTitle}
+      onChange={(e) => setNewTitle(e.target.value)}
+      onKeyDown={updateTitleHandler}
+      autoFocus
+    />
+  );
+};
+
+export default TitleInput;
