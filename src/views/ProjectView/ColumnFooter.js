972bbf1c01e0d3b82237158a3bc9a43883d1b612
Wiktor Jagiełło <wiktor.jagiello93@gmail.com>
2021-02-23 18:04:45 +0100

    Add feature to delete columns

--- src/views/ProjectView/ColumnFooter.js
+++ src/views/ProjectView/ColumnFooter.js
@@ -3,19 +3,14 @@ import firebase from 'firebase/app';
 const ColumnFooter = ({ index, data }) => {
   const deleteColumn = (index) => {
     const id = data[0].id;
+    const newData = data[0].board.project0.projectContent;
+    delete newData[index];
 
     firebase
       .firestore()
       .collection('users')
       .doc(id)
-      .set(
-        {
-          'board.project0.projectContent': {
-            [index]: firebase.firestore.FieldValue.delete(),
-          },
-        },
-        { merge: true }
-      )
+      .update({ 'board.project0.projectContent': newData })
       .then(() => {
         console.log('Document successfully updated!');
       });
@@ -25,7 +20,6 @@ const ColumnFooter = ({ index, data }) => {
       <div className="add-task">+ Dodaj kolejną task</div>
       <div className="column-options">
         <button onClick={() => deleteColumn(index)}>❌</button>
-        {/* <button onClick={() => console.log(index)}>❌</button> */}
       </div>
     </div>
   );
