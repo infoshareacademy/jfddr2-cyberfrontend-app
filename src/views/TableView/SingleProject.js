47cc381a9e559ffa4870b2ce282eba638db8fc07
Wiktor Jagiełło <wiktor.jagiello93@gmail.com>
2021-02-23 17:58:49 +0100

    coment out console.log

--- src/views/TableView/SingleProject.js
+++ src/views/TableView/SingleProject.js
@@ -1,7 +1,7 @@
-import "./SingleProject.css";
+import './SingleProject.css';
 
 function SingleProject({ data }) {
-  console.log(data);
+  // console.log(data);
   return (
     <div className="single-project">
       <p>{data.projectName}</p>
