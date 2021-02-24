c809c1d0d1e2c1fbfa2150871fda52afc76cb3aa
Wiktor Jagiełło <wiktor.jagiello93@gmail.com>
2021-02-24 20:14:46 +0100

    Add userId and projectId

--- src/views/TableView/NewProjectBtn.js
+++ src/views/TableView/NewProjectBtn.js
@@ -1,23 +1,21 @@
-import "./NewProjectBtn.css";
-import firebase from "firebase/app";
-import "firebase/firestore";
+import './NewProjectBtn.css';
+import firebase from 'firebase/app';
+import 'firebase/firestore';
 
-function NewProjectBtn({ data }) {
-  console.log(data[0]);
-  // console.log(data[0].id);
-  // const id = data[0].id; //id pierwszego usera
+function NewProjectBtn({ data, userId }) {
+  // const projectId = 'project' + timeStamp;
   // firebase
   //   .firestore()
   //   .collection("users")
-  //   .doc(id)
+  //   .doc(userId)
   //   .update({
   //     board: {
   //       ...data[0].board,
-  //       project4: {
+  //       projectId: {
   //         //jeśli chcesz dodać nowy project tutaj musisz zmienić nazwę klucza. Jeśli chcesz wstawiać go używając stanu inputa to musisz zrobić taki myk: [stanInputa] : {...
   //         archive: false, //jeśli chcesz edytować obiekt to nazwa klucza musi być taka sama i wstawiasz wszystkie dane obiektu wraz z tymi zmienionymi
   //         favourite: false,
-  //         projectId: 69,
+  //         projectId,
   //         projectName: "Project 0",
   //         projectContent: {},
   //       },
